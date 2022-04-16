9750d650f8cc1135984505e221c845d2a1313743
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-04-16 01:27:58 +0100

    Replaced sum ternaries with ampersand in CharacterCard.jsx

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -13,13 +13,13 @@ const CharacterCard = ({character, index}) => {
         : <img className="card__image" src={noImageAvailable} alt="No Image Available"/>}
 
       <div className="character-details">
-        {species ? <p className="trait"><b>Species: </b>{species}</p> : <></>}
-        {gender ? <p className="trait"><b>Gender: </b>{gender}</p> : <></>}
-        {house ? <p className="trait"><b>House: </b>{house}</p> : <></>}
-        {ancestry ? <p className="trait"><b>Ancestry: </b>{ancestry}</p> : <></>}
-        {wand.length ? <p className="trait"><b>Wand: </b>{wand.wood + 
-          " " + wand.core + " " + wand.length}</p> : <></>}
-        {yearOfBirth ? <p className="trait"><b>Year Of Birth: </b>{yearOfBirth}</p> : <></>}
+        {species && <p className="trait"><b>Species: </b>{species}</p>}
+        {gender && <p className="trait"><b>Gender: </b>{gender}</p>}
+        {house && <p className="trait"><b>House: </b>{house}</p>}
+        {ancestry && <p className="trait"><b>Ancestry: </b>{ancestry}</p>}
+        {wand.length && <p className="trait"><b>Wand: </b>{wand.wood + 
+          " " + wand.core + " " + wand.length}</p>}
+        {yearOfBirth && <p className="trait"><b>Year Of Birth: </b>{yearOfBirth}</p>}
       </div>
 
     </article>
